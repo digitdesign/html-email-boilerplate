15f0f1993fea0cc1b238844ed13301bb45c6574a
Shaan Shivanandan <shaan.shivanandan@godigit.com>
2018-02-17 00:06:53 +0530

    Chore: Added GruntJS workflow

---
+++ _build/gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function (grunt) {
+
+	/*Project configuration*/
+	grunt.initConfig({
+
+		/*Metadata*/
+		meta: {
+			srcPath: '../src/',
+			deployPath: '../dist/',
+			buildPath: '../_build/'
+		},
+		autoprefixer: {
+			dynamic_mappings: {
+				expand: true,
+				cwd: '<%= meta.srcPath %>css/',
+				src: '*.css',
+				dest: '<%= meta.deployPath %>css/'
+			}
+		},
+		csscomb: {
+			options: {
+				config: '<%= meta.buildPath %>config/.csscomb.json'
+			},
+			dynamic_mappings: {
+				expand: true,
+				cwd: '<%= meta.srcPath %>css/',
+				src: '*.css',
+				dest: '<%= meta.deployPath %>css/'
+			}
+		}
+	});
+
+	/*Tasks*/
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-csscomb');
+
+	/*Default task*/
+	grunt.registerTask('default', ['autoprefixer']);
+};
